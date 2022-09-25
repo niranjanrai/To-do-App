5626d0e0ce3e893ecd4e6fab48190027b177d808
niranjanrai <rainiranjan1998@gmail.com>
2022-09-26 01:47:05 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const todoForm = document.querySelector(".form-todo");
 const todoInput = document.querySelector(".form-todo input[type='text']");
-console.log(todoInput);
+const todoList = document.querySelector(".todo-list");
+console.log(todoList);
+
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  const newTodoText = todoInput.value;
+  const newLi = document.createElement("li");
+  const newLiInnerHtml = `
+  <span class="text"> ${newTodoText}</span>
+  <div class="todo-buttons">
+      <button class="todo-btn done">Done</button>
+      <button class="todo-btn remove">Remove</button>
+  </div>
+
+`;
+  newLi.innerHTML = newLiInnerHtml;
+  todoList.append(newLi);
   todoInput.value = "";
+});
+
+todoList.addEventListener("click", (e) => {
+  // checked if user clicked on done button
+  if (e.target.classList.contains("remove")) {
+    const targetedLi = e.target.parentNode.parentNode;
+    targetedLi.remove();
+  }
+  if (e.target.classList.contains("done")) {
+    const liSpan = e.target.parentNode.previousElementSibling;
+    liSpan.style.textDecoration = "line-through";
+  }
 });
